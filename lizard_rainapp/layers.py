04780255c2e7284960e6397c8354b81493fe4f71
jack.ha <jack.ha@476a50c0-da5d-5340-8c75-4ad663a144f1>
2011-08-25 11:37:25 +0000

    added quite empty adapter.html function with template

--- lizard_rainapp/layers.py
+++ lizard_rainapp/layers.py
@@ -1,4 +1,19 @@
+from django.template.loader import render_to_string
+
 from lizard_fewsjdbc.layers import FewsJdbc
 
+
 class RainAppAdapter(FewsJdbc):
-    pass
+    """
+    Adapter for Rain app.
+    """
+
+    def html(self, snippet_group=None, identifiers=None, layout_options=None):
+
+        symbol_url = self.symbol_url()
+        img_url = ""
+
+        return render_to_string(
+            'lizard_rainapp/popup_rainapp.html',
+            {'symbol_url': symbol_url,
+             'img_url': img_url})
