2aeb09664789b9794e818e21a8baef17414ac750
jack.ha <jack.ha@476a50c0-da5d-5340-8c75-4ad663a144f1>
2011-08-25 13:03:45 +0000

    developing table, added url params to bar_url and img_url

--- lizard_rainapp/layers.py
+++ lizard_rainapp/layers.py
@@ -1,10 +1,12 @@
 import datetime
+import json
 
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
 
 from lizard_fewsjdbc.layers import FewsJdbc
 from lizard_map.adapter import Graph
+from lizard_map.daterange import current_start_end_dates
 
 
 class RainAppAdapter(FewsJdbc):
@@ -29,6 +31,15 @@ class RainAppAdapter(FewsJdbc):
         return graph.http_png()
 
 
+    def rain_stats(self, identifier, td, start_date, end_date):
+        return {
+            'td': td,
+            'max': 'max',
+            'start': 'start',
+            'end': 'end',
+            't': 'herhalingstijd'}
+
+
     def html(self, snippet_group=None, identifiers=None, layout_options=None):
         """
         Popup with graph - table - bargraph.
@@ -37,8 +48,20 @@ class RainAppAdapter(FewsJdbc):
             identifiers = [
                 snippet.identifier
                 for snippet in snippet_group.snippets.filter(visible=True)]
-        # TODO: make table with these identifiers
+        title = 'RainApp (%s)' % ', '.join(
+            [identifier['location'] for identifier in identifiers])
 
+        # Make table with these identifiers.
+        start_date, end_date = current_start_end_dates(
+            layout_options['request'])
+        rain_stats = []  # Contains dicts with 'max', 'start', 'end', '..'
+
+        for identifier in identifiers:
+            rain_stats.append(self.rain_stats(
+                    identifier, datetime.timedelta(days=2),
+                    start_date, end_date))
+
+        # Collect urls for graphs.
         symbol_url = self.symbol_url()
         if snippet_group:
             # Image url for snippet_group: can change if snippet_group
@@ -62,10 +85,17 @@ class RainAppAdapter(FewsJdbc):
                 "lizard_rainapp.workspace_item_rainapp_bars",
                 kwargs={'workspace_item_id': self.workspace_item.id},
                 )
+            identifiers_escaped = [json.dumps(identifier).replace('"', '%22')
+                                   for identifier in identifiers]
+            url_extra = '?' + '&'.join(['identifier=%s' % i for i in
+                                        identifiers_escaped])
+            img_url += url_extra
+            bar_url += url_extra
 
         return render_to_string(
             'lizard_rainapp/popup_rainapp.html',
-            {'title': 'title',
+            {'title': title,
+             'rain_stats': rain_stats,
              'symbol_url': symbol_url,
              'img_url': img_url,
              'bar_url': bar_url})
