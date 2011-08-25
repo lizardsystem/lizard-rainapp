a0605ca8a8bd5ac1ee19a591a69647d26b8a215f
jack.ha <jack.ha@476a50c0-da5d-5340-8c75-4ad663a144f1>
2011-08-25 14:18:57 +0000

    added first working rain statistics

--- lizard_rainapp/layers.py
+++ lizard_rainapp/layers.py
@@ -1,12 +1,16 @@
 import datetime
 import json
+import logging
 
+from django.core.cache import cache
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
 
 from lizard_fewsjdbc.layers import FewsJdbc
 from lizard_map.adapter import Graph
 from lizard_map.daterange import current_start_end_dates
+
+logger = logging.getLogger(__name__)
 
 
 class RainAppAdapter(FewsJdbc):
@@ -30,16 +34,58 @@ class RainAppAdapter(FewsJdbc):
 
         return graph.http_png()
 
+    # def _cached_values(self, identifier, start_date, end_date):
+    #     """
+    #     Same as self.values, but cached.
+
+    #     The stored values are rounded in days, a 'little bit
+    #     more'. Else the cache will always miss.
+    #     """
+    #     start_date_cache = datetime.datetime(
+    #         start_date.year, start_date.month, start_date.day)
+    #     end_date_cache = (
+    #         datetime.datetime(
+    #             end_date.year, end_date.month, end_date.day
+    #             ) + datetime.timedelta(days=1))
+    #     cache_key = hash('%s::%s::%s::%s::%s::%s' % (
+    #             self.jdbc_source.id, self.filterkey, self.parameterkey,
+    #             identifier['location'], start_date_cache, end_date_cache))
+    #     values = cache.get(cache_key)
+    #     if values is None:
+    #         logger.debug('Caching values for %s' % identifier['location'])
+    #         values = self.values(identifier, start_date, end_date)
+    #         cache.set(cache_key, values)
+
+    #     print start_date
+    #     print values[0]['datetime']
+    #     # Remove datetimes out of range.
+    #     # Problem: tz aware and tz naive datetimes cannot be compared
+    #     while values and values[0]['datetime'] < start_date:
+    #         del values[0]
+    #     while values and values[-1]['datetime'] > end_date:
+    #         del values[-1]
+
+    #     return values
 
     def rain_stats(self, identifier, td, start_date, end_date):
         """
-        Calculate stats
+        Calculate stats.
+
+        TODO: make faster by caching the data.
         """
+        check_start = start_date - td
+        check_end = start_date
+        values = self.values(identifier, check_start, check_end)
+        if values:
+            max_value = max(values, key=lambda i: i['value'])
+        else:
+            max_value = {'value': 'geen waardes', 'datetime': '-'}
         return {
             'td': td,
-            'max': 'max',
-            'start': 'start',
-            'end': 'end',
+            'max': max_value['value'],
+            'datetime': max_value['datetime'],
+            'start': check_start,
+            'end': check_end,
             't': 'herhalingstijd'}
 
 
@@ -60,10 +106,17 @@ class RainAppAdapter(FewsJdbc):
             layout_options['request'])
         rain_stats = []  # Contains dicts with 'max', 'start', 'end', '..'
 
+        td_range = [
+            datetime.timedelta(days=2),
+            datetime.timedelta(days=1),
+            datetime.timedelta(hours=3),
+            datetime.timedelta(hours=1)
+            ]
         for identifier in identifiers:
-            rain_stats.append(self.rain_stats(
-                    identifier, datetime.timedelta(days=2),
-                    start_date, end_date))
+            for td in td_range:
+                rain_stats.append(self.rain_stats(
+                        identifier, td,
+                        start_date, end_date))
 
         # Collect urls for graphs.
         symbol_url = self.symbol_url()
